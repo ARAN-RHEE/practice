173de85c9206944f1dddbde89e059aced5240a00
ARAN-RHEE <aranrheearanrhee@gmail.com>
2019-03-13 17:18:56 +0900

    step05

--- src/step05/test3.java
+++ src/step05/test3.java
@@ -5,11 +5,22 @@ public class test3 {
 		
 		String abc = "abc";
 		String abc1 = "abc";
+		String abc2 = new String("abc");
 		
 		if(abc==abc1) {
-			System.out.println("true");
+			System.out.println("1. true");
 		} else {
-			System.out.println("false");
+			System.out.println("1. false");
 		}
+		
+		
+		if(abc == abc2) {
+			System.out.println("2. true");
+		} else {
+			System.out.println("2. false");
+		}
+		
+		System.out.println("3. " + abc2.equals(abc));
+		System.out.println("3. " + abc2.equals(abc1));
 	}
 }
