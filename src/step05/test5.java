0d7559de36a624ecd78ae022eca61dc2157de81e
ARAN-RHEE <aranrheearanrhee@gmail.com>
2019-03-13 18:23:28 +0900

    배열

---
+++ src/step05/test5.java
@@ -0,0 +1,19 @@
+package step05;
+
+public class test5 {
+
+	public static void main(String[] args) {
+
+		int score[] = new int[20];
+		
+		for(int i=0; i<20;i++) {
+			
+			score[i]=(int) (Math.random()*99);
+			
+		System.out.println(score[i]);	
+		}
+		
+	}
+	
+}	
+
